7ccf4778b86343fd2be3bf06f7897751db4dcb15
DMVella <d.vella@unsw.edu.au>
2026-01-20 14:06:00 +1100

    Maple: Adds I to previewer, old *.mla deleted
    make.bat now deletes old *.mla files before
    generating new ones, and TestingThePreviewer.mpl
    is included in the build.
    
    create_MathML() now includes code to replace
    variable `I` with `i` for previewing purposes
    (so that 3*I is displayed as \(3i\) rather than
    \(3\times I\)).
    
    Replaces search-and-replace for `Pi` -> `pi` with
    RegSubs() instead of SubstituteAll().
    
    Adjustments to wording in semantic advice for
    consistency, in the cases of:
    	- variable `e`
    	- function `In(anything)`
    	- variable `pi`
    	- variable `PI`

--- Maple/make.bat
+++ Maple/make.bat
@@ -2,6 +2,8 @@ REM Batch script to create zip files for .ind and .lib files
 REM Credit: Generated with the assistance of ChatGPT from OpenAI (May 24 Version)
 
 @echo off
+DEL *.mla
 cmaple MaplePreviewCode.mpl
+cmaple TestingThePreviewer.mpl
 
 pause
